53722c6e1a772204e4cd34c9834e2a3468b2e588
piratheesan <vijayapirathee04@gmail.com>
2019-10-10 04:19:41 +0530

    Retreived data from project table to form

--- src/Component/Project.jsx
+++ src/Component/Project.jsx
@@ -2,19 +2,46 @@ import React, { Component } from 'react'
 import './Style.css'
 import Head from './Header.jsx'
 import Footer from './Footer.jsx'
+import Axios from 'axios';
 
 export default class Project extends Component {
+
+    state = {
+        projects: []
+      }
+    
+        componentDidMount() {
+        Axios.get(`http://localhost:8080/dt/api/v1/project`)
+          .then(res => {
+            console.log(res);
+             this.setState({ projects:res.data });
+             console.log( this.setState);
+          })
+          this.refreshProject();
+    }
+    refreshProject() {
+        Axios.get("http://localhost:8080/dt/api/v1/project")
+        .then(res => {
+        //   console.warn("Refresh Service is working");
+          this.setState({ projects:res.data });
+        });
+      }
+      
+        
+
     render() {
+        console.log('hhh'+this.state.projects)
         return (
             <div>
             <Head />
-                <div className="LeftNav">
+                <div className="LeftNav">           
+
                 <h2>Add Project</h2>
                 <form>
                     <label>Name</label><br></br>
-                    <input type="text" name="name" className="txtid"></input><br></br><br></br>     
+                    <input type="text" name="projectName" className="txtid" ></input><br></br><br></br>     
                     <label>Description</label> <br></br>                 
-                    <input type="text" name="type" className="txtid"></input><br></br><br></br>
+                    <input type="text" name="projectDescription" className="txtid" ></input><br></br><br></br>
                     <button className="save_but" type="submit"><i className="fa fa-save" >
                     &nbsp;Save</i></button>                                         
                     &emsp;<input type="reset" Value="Reset" className="reset_but"/> 
@@ -30,10 +57,12 @@ export default class Project extends Component {
                     <th>Name</th>                        
                     <th>Action</th>                    
                 </tr>
-                <tr>
-                    <td>ID</td>
-                    <td>Description</td>
-                    <td>Name</td>
+                { this.state.projects.map(p=>{
+                 return(                
+                   <tr>
+                    <td>{p.projectId}</td>
+                    <td>{p.projectDescription}</td>
+                    <td>{p.projectName}</td>
                     <td>&nbsp;&nbsp;<button className="edit">
                     <i className="fa fa-edit" >
                     &nbsp;Edit</i></button>
@@ -42,13 +71,13 @@ export default class Project extends Component {
                     &nbsp;Delete
                     </i>
                     </button></td>
-                </tr>
-                
-                 </table>                                        
+                </tr>) 
+                }) }
+                 </table>                                               
                 
                 </div>
                 <Footer />
             </div>
-        )
+        );
     }
 }
